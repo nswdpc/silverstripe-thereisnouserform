c1c1fd6fbaf27b638d86fe397cc434c947855d3d
JamesDPC <69664712+JamesDPC@users.noreply.github.com>
2026-04-15 07:43:30 +0000

    [rector] Automated updates generated by rector configuration

--- src/Extensions/UserDefinedFormPageExtension.php
+++ src/Extensions/UserDefinedFormPageExtension.php
@@ -1,11 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace NSWDPC\Utilities\ThereIsNoUserform;
 
 use SilverStripe\ORM\DataExtension;
 
 /**
  * @author James
+ * @extends \SilverStripe\ORM\DataExtension<(\SilverStripe\UserForms\Model\UserDefinedForm & static)>
  */
 class UserDefinedFormPageExtension extends DataExtension {
 

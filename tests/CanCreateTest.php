0b02c5834743786384417261a8eb1a2904ab7e48
James (PD) <69664712+JamesDPC@users.noreply.github.com>
2026-07-24 09:48:19 +1000

    Add a test

---
+++ tests/CanCreateTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NSWDPC\Utilities\ThereIsNoUserform\Tests;
+
+use NSWDPC\Utilities\ThereIsNoUserform\UserDefinedFormPageExtension;
+use SilverStripe\Dev\SapphireTest;
+use SilverStripe\UserForms\Model\UserDefinedForm;
+
+/**
+ * Test can create configuration
+ * @author James
+ */
+class CanCreateTest extends SapphireTest
+{
+
+    protected $usesDatabase = true;
+
+    public function testCannotCreateUserDefinedForm(): void
+    {
+        $inst = UserDefinedForm::create();
+        $this->assertTrue($inst->hasExtension(UserDefinedFormPageExtension::class));
+        $this->assertFalse($inst->canCreate());
+    }
+
+
+}
